b07110c19dc4cb8bf1ff241a70e20d5976a0b1d2
amnayousufiqbal <amnayousuf2002@gmail.com>
2024-08-11 22:39:00 +0500

    Assigenmnt 5

---
+++ Assigenmant5_Q1.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> names = ["Amna", "Aisha", "Annie", "Aliin"];
+
+  print(names[0]);
+  print(names[1]);
+  print(names[2]);
+  print(names[3]);
+}
---
+++ Assigenmant5_Q3.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> studentInfo = ["Amna Iqbal", "10th Grade", "12345", "A", "85%"];
+  print("Name: ${studentInfo[0]}");
+  print("Class: ${studentInfo[1]}");
+  print("Roll No: ${studentInfo[2]}");
+  print("Grade: ${studentInfo[3]}");
+  print("Percentage: ${studentInfo[4]}");
+}
